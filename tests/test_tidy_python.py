be7599102d4c4fc76bceeea0c8062d687041afe5
Nathan Drew <1035229+NaffanDroo@users.noreply.github.com>
2022-12-10 15:33:48 +0000

    fix: temporarily remove this test

--- tests/test_tidy_python.py
+++ tests/test_tidy_python.py
@@ -2,4 +2,5 @@ from tidy_python import __version__
 
 
 def test_version():
-    assert __version__ == "0.1.4"
+    assert __version__
+    # assert __version__ == "0.1.4" until https://github.com/googleapis/release-please/issues/1773 is fixed
